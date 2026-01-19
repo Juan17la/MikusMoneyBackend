b0fd87432d0d3a5af6f2634155314d7c349757c3
Jud1714 <juandiegolopezarias07@gmail.com>
2026-01-19 11:09:55 -0500

    Add Maven Wrapper and initial project configuration files

---
+++ src/test/java/com/example/mikusmoneybackend/MikusmoneybackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mikusmoneybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MikusmoneybackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
