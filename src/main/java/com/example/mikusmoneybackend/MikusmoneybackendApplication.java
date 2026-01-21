520cd6fc3526a06cf84980fb0dccd875d0cc55e9
Jud1714 <juandiegolopezarias07@gmail.com>
2026-01-21 11:05:49 -0500

    Add MikusmoneybackendApplication class as the entry point for the application

---
+++ src/main/java/com/example/mikusmoneybackend/MikusmoneybackendApplication.java
@@ -0,0 +1,16 @@
+package com.example.mikusmoneybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MikusmoneybackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MikusmoneybackendApplication.class, args);
+		System.out.println("==================================");
+		System.out.println("Mikus Money Backend is running...");
+		System.out.println("==================================");
+	}
+
+}
